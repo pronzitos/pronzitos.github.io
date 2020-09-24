889a9db0d6ff3d160459d586ab950f90ae8afa98
pronzitos <andrea.pronzati16@gmail.com>
2020-09-24 23:00:47 +0200

    today

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {scrollFunction()};
+/*window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -6,4 +6,26 @@ function scrollFunction() {
   } else {
     document.getElementById("return").style.fontSize = "5vw";
   }
-}
+}*/
+
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+      margin: 10,
+            nav: true,
+      navText: [
+          'PREV',
+          'NEXT'
+      ],
+      slideSpeed : 300,
+      paginationSpeed : 100,
+      items : 1, 
+      itemsDesktop : false,
+      itemsDesktopSmall : false,
+      itemsTablet: false,
+      itemsMobile : false,
+      animateOut: 'fadeOut'
+  });
+ 
+});
